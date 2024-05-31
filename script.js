8fa5a691129e31de2776a17d2ba90f9e0d7e8323
Prunebsn <p.besson@eodd.fr>
2024-05-31 21:01:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -105,5 +105,4 @@ fetch('geojson/Pts_GR4J.geojson')
         });
 
         // Ajouter la couche des stations au-dessus des autres couches
-        geojsonMarkerOptions.addTo(map);
-    });
+        stationsLayer.addTo(map);
